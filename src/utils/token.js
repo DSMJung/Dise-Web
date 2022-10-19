279165af081efe70ce79841bae965aba41ab6cff
안윤지 <dksdbswl10430@naver.com>
2022-10-20 00:28:41 +0900

    💻 :: 토큰 삭제 & 토큰 설정

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,3 +1,11 @@
 export const getAccessToken = () => {
   return localStorage.getItem('access_token');
 };
+
+export const deleteAccessToken = () => {
+  localStorage.removeItem('access_token');
+};
+
+export const setAccessToken = (token) => {
+  localStorage.setItem('access_token', token);
+};
